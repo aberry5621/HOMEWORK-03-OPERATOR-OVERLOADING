96a18d4d6f3ae04c153df8f05091c64976373b97
aberry5621 <aberry5621@gmail.com>
2017-02-17 08:57:20 -0800

    Initial Commit

---
+++ HOMEWORK 03 OPERATOR OVERLOADING/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HOMEWORK 03 OPERATOR OVERLOADING
+//
+//  Created by ax on 2/17/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
